4605e28f5f86e29011f344bc2a609fd3619be09a
kanbaru-github <usupfv@gmail.com>
2023-12-17 11:04:23 +0900

    Installed Sass

--- nextjs-dashboard/next.config.js
+++ nextjs-dashboard/next.config.js
@@ -2,3 +2,12 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+/** @type {import('path')} pathモジュール */
+const path = require('path');
+
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')], // Sassファイル内で使用するパスを指定
+  },
+}
